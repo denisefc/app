9fb53efa6f64bbd97dcc26475ccb820d59b0b4b9
Denise FC <deniseuff@gmail.com>
2024-06-27 11:52:12 -0300

    aula 05b - parte 01

--- deniseAPI/src/main/java/br/edu/infnet/deniseAPI/clients/MoedaClient.java
+++ deniseAPI/src/main/java/br/edu/infnet/deniseAPI/clients/MoedaClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Collection;
 
-@FeignClient(url = "https://economia.awesomeapi.com.br/json", name = "cotação")
+@FeignClient(url = "https://economia.awesomeapi.com.br/json", name = "cotacao")
 public interface MoedaClient {
     @GetMapping(value = "/available/uniq")
     Collection<Moeda> obterMoedas();
